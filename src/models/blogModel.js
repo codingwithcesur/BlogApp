faae1f491d0faccbc2eb1e0296bd830b95870302
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-10-18 20:28:05 +0300

    add BlogPost model

---
+++ src/models/blogModel.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const blogPostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    published: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { collection: "blogPosts", timestamps: true }
+);
+
+module.exports = {
+  BlogPost: mongoose.model("BlogPost", blogPostSchema),
+};
