0a3a8f0712e2293d8d5fedd4bbb8e83f4c8f68bd
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-10-25 21:40:29 +0300

    add login authorization check

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,4 +7,6 @@ const { User } = require("../controllers/userController");
 router.route("/").get(User.list).post(User.create);
 router.route("/:userId").get(User.read).put(User.update).delete(User.delete);
 
+// ------------------------------------------------
+router.post("/login", User.login);
 module.exports = router;
